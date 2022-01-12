11937d2a42300c920073587ada07dadb25b6871c
Christian Gerber <cgerber@rbauction.com>
2022-01-11 16:15:34 -0800

    add something to commit

--- MovizTests/MovizTests.swift
+++ MovizTests/MovizTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Moviz
 
 class MovizTests: XCTestCase {
-
+    // make an arbitrary change to create something to commit
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
